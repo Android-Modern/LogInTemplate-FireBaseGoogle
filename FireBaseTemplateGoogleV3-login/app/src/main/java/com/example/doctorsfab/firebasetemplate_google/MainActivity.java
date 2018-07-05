230dd7faa81938c88e9bfa785a8a7144746f2cf5
qwertyu5258 <qwertyu5258@naver.com>
2018-07-05 18:10:28 +0900

    2

---
+++ FireBaseTemplateGoogleV3-login/app/src/main/java/com/example/doctorsfab/firebasetemplate_google/MainActivity.java
@@ -0,0 +1,147 @@
+package com.example.doctorsfab.firebasetemplate_google;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    String tag = "Tag";
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    public TextView textView1;
+    public TextView textView2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView1 = findViewById(R.id.editText1);
+        textView2 = findViewById(R.id.editText2);
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user != null) {
+                    Log.d(tag, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    Log.d(tag, "onAuthState:signed_out");
+                }
+            }
+        };
+
+
+        Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                createAccount("qwertyu5258@naver.com","gpfla156");
+                createAccount(textView1.getText().toString(), textView2.getText().toString());
+
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private boolean isValidPasswd(String target) {
+
+        Pattern p = Pattern.compile("(^.*(?=.{6,100})(?=.*[0-9])(?=.*[a-zA-Z]).*$)");
+
+        Matcher m = p.matcher(target);
+        if (m.find() && !target.matches(".*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*")) {
+            return false;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean isValidEmail(String target) {
+        if (target == null || TextUtils.isEmpty(target)) {
+            return false;
+        } else {
+            return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+        }
+    }
+
+
+    private void createAccount(String email, String password) {
+        if (!isValidEmail(email)) {
+            Log.e(tag, "createAccount: email is not valid ");
+            Toast.makeText(MainActivity.this, "Email is not valid",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (isValidPasswd(password)) {
+            Log.e(tag, "createAccount: password is not valid ");
+            Toast.makeText(MainActivity.this, "Password is not valid",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        Log.d(tag, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Authentication failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // [START_EXCLUDE]
+                        // [END_EXCLUDE]
+                    }
+                });
+
+    }
+
+}
